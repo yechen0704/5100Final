51c3ef831ae1c5a878f5c706a04145143fbcd382
Uni-boy <liu.xinzh@northeastern.edu>
2021-12-10 15:36:53 -0500

    main

---
+++ AirlineSystem/src/Business/Role/Role.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+
+import Business.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public abstract class Role {
+    
+    public enum RoleType{
+        AirlineAdmin("AirlineAdmin"),
+        Customer("Customer"),
+        Ackemma("Ackemma"),
+        ClaimSettler("ClaimSettler"),
+        SysAdmin("Sysadmin");
+        
+        private String value;
+        private RoleType(String value){
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+            UserAccount account,  
+            EcoSystem business);
+
+    @Override
+    public String toString() {
+        return this.getClass().getName();
+    }
+    
+    
+}
